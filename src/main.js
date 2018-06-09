bef8453872440477402faafb14da10416f380c74
ak232 <ajay.ashubaba@gmail.com>
2018-06-09 18:06:47 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
+
+import Quasar, * as All from "quasar-framework/dist/quasar.mat.esm.js";
+
 import App from "./App";
+import Router from "./router";
+import Store from "./store";
+
+import { MagicPluginLoader } from "./helpers";
+
+import "quasar-framework/dist/quasar.mat.styl";
+import "quasar-extras/material-icons.js";
+import "./css/styles.mat.styl";
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
+// init quasar
 Vue.use(Quasar, {
   components: All,
   directives: All,
@@ -17,4 +30,10 @@ const app = {
   store: Store,
   ...App
 };
+
+// init user plugins
+MagicPluginLoader("i18n", app);
+MagicPluginLoader("axios", app);
+
+/* eslint-disable no-new */
 new Vue(app);
